d1aba9e4ea0b9462f381653f77e2dc03df6422c4
lyzgit <youzel@126.com>
2018-03-05 22:29:21 +0800

    code

---
+++ src/easy/num771/Solution.java
@@ -0,0 +1,21 @@
+package easy.num771;
+
+/**
+ * @author youzeliang
+ * on 2018/3/5
+ */
+public class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int[] count = new int[64];
+        for (char c : J.toCharArray()) {
+            count[c - 'A']++;
+        }
+        int ans = 0;
+        for (char c : S.toCharArray()) {
+            if (count[c - 'A'] >= 1) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
