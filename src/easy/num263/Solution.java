5e1d8e2823498871a8a21e85fe0947fd9cc4cc7b
yzrds <youzel@126.com>
2018-07-18 21:32:22 +0800

    o

--- src/easy/num263/Solution.java
+++ src/easy/num263/Solution.java
@@ -21,9 +21,13 @@ public class Solution {
         while (num % 2 == 0) {
             num /= 2;
         }
-        while (num%3==0){
-            num
+        while (num % 3 == 0) {
+            num /= 3;
         }
+        while (num % 5 == 0) {
+            num /= 3;
+        }
+        return num == 1;
     }
 
 }
