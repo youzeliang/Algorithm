19dc4b2727f4f1de463824d89b09755d9a65c6c6
yzrds <youzel@126.com>
2018-07-19 21:02:47 +0800

    solution

--- src/easy/num263/Solution.java
+++ src/easy/num263/Solution.java
@@ -4,14 +4,13 @@ package easy.num263;
  * Created by youze on 18-7-18
  * <p>
  * https://leetcode.com/problems/ugly-number/description/
+ * <p>
+ * 其质数因子只能是2,3,5,所以就不断的除以２,3,5，结果等于１就是丑陋数了
  */
 public class Solution {
 
-    public static void main(String[] args) {
-        System.out.println(isUgly(14));
-    }
 
-    public static boolean isUgly(int num) {
+    public boolean isUgly(int num) {
         if (num <= 0) {
             return false;
         }
@@ -22,7 +21,7 @@ public class Solution {
             num /= 3;
         }
         while (num % 5 == 0) {
-            num /= 3;
+            num /= 5;
         }
         return num == 1;
     }
---
+++ src/easy/num709/Solution.java
@@ -0,0 +1,20 @@
+package easy.num709;
+
+
+/**
+ * Created by youze on 18-7-19
+ * https://leetcode.com/problems/to-lower-case/description/
+ * <p>
+ * 字符串返回小写，可以先转化为数组，然后遍历转换
+ */
+public class Solution {
+
+    public String toLowerCase(String str) {
+        char chrs[] = str.toCharArray();
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            chrs[i] = Character.toLowerCase(chrs[i]);
+        }
+        return str.toLowerCase();
+    }
+}
