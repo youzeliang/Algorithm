ed4854d8077b7282ac5bdeee3704b41104399650
yzrds <youzel@126.com>
2018-10-04 17:31:58 +0800

    num 371

---
+++ src/LeetCode/easy/num371/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.easy.num371;
+
+/**
+ * Created By Youze on 10/4/2018
+ */
+public class Solution {
+
+    public  int getSum(int a, int b) {
+        if(a == 0) return b;
+        int x = a ^ b;
+        int c = (a & b) << 1;
+        return getSum(c, x);
+    }
+}
