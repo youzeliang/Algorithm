2f77cbf9da8f667406c0177172f1c1ca1df44154
yzrds <youzel@126.com>
2018-10-16 23:50:41 +0800

    num  258 Add Digits

--- src/LeetCode/easy/num258/Solution.java
+++ src/LeetCode/easy/num258/Solution.java
@@ -15,6 +15,6 @@ package LeetCode.easy.num258;
 public class Solution {
 
     public int addDigits(int num){
-        return num % 9 == 0? num == 0? 0 : 9 : num % 9;
+        return num == 0 ? 0 : (num % 9 == 0 ? 9 : num % 9);
     }
 }
