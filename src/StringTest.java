dac5dd3c797acc4f5c9e232cb2e5e2e5360c719e
yzrds <youzel@126.com>
2018-11-05 23:04:45 +0800

    num 992 Sort Array By Parity II

--- src/StringTest.java
+++ src/StringTest.java
@@ -1,16 +1,5 @@
 
 public class StringTest {
     public static void main(String[] args) {
-        /*() findViewById(R.id.);
-        Callable<Object> callable = new Callable<Object>() {
-            public Object call() throws Exception {
-
-
-            }
-        };*/
-
-
     }
-
-
 }
